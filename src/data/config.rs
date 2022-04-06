e3734bfb6a66181b767a90b0a2d23443bcf13df3
Eduardo Flores <edfloreshz@gmail.com>
2022-04-06 09:55:25 -0700

    Removed references to Microsoft To Do client

--- src/data/config.rs
+++ src/data/config.rs
@@ -1,1 +1,19 @@
 
+use crate::services::microsoft::service::MicrosoftService;
+use crate::services::ToDoService;
+
+pub struct Settings {}
+
+impl Settings {
+    pub fn config() -> Result<()> {
+        let mut config = Config::new("do")
+            .about("Do is a To Do app for Linux built with Rust and GTK.")
+            .author("Eduardo Flores")
+            .version("0.1.0")
+            .write()?;
+        if !MicrosoftService::is_token_present() {
+            MicrosoftService::create_config(&mut config)?;
+        }
+        Ok(())
+    }
+}
